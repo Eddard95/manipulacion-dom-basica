dddda6c558a3e0e75b21bb5ca2895493aeae2b7c
Eddard95 <edwardye666@hotmail.com>
2022-10-22 18:11:41 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,17 @@
+// .parrafito -- Clase
+// #pid -- id
+
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#calcular');
+const pResult = document.querySelector('#result');
+
+btn.addEventListener('click', btnOnClick)
+
+
+function btnOnClick(){
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado " + sumaInputs;
+}
+
